89b143f36a31f96743c2a82acba412c59e725701
Jean5011 <jeanpierreesquen@gmail.com>
2025-06-01 19:02:57 -0300

    punto 1,2 y5

--- TP_TEMA6_GRUPO3/src/main/java/frgp/utn/edu/ar/Main.java
+++ TP_TEMA6_GRUPO3/src/main/java/frgp/utn/edu/ar/Main.java
@@ -19,10 +19,15 @@ public class Main
     public static void main( String[] args ){
     	Confighiberneate ch = new Confighiberneate();
     	Session session = ch.abrirConexion();
-    	//Mostrar todos los médicos ordenados según su legajo de mayor a menor.
-    	
+    	List<Medicos> lista =(List<Medicos>) session.createQuery("FROM Medicos m ORDER BY m.id DESC").list();
+    	for (Medicos legajo : lista) {
+    		System.out.println(legajo.toString());
+    	}
     	//Mostrar todos los médicos ordenados según su legajo de menor a mayor.
-    	
+    	List<Object[]> obj =(List<Object[]>) session.createQuery("SELECT m.legajo, m.nombre, m.apellido FROM medico m ORDER BY m.legajo ASC").list();
+    	for (Object[] doc : obj) {
+    		System.out.println(doc[0]+" " + doc[1]+" " + doc[2]);
+    	}
     	
     	//Mostrar todos los turnos que posee el médico con legajo 1234 en el día 01/01/2025
     	
@@ -34,6 +39,8 @@ public class Main
     	}
     	
     	//Mostrar el médico con mayor número de legajo
+    	Medicos doctor = (Medicos) session.createQuery("SELECT MAX(m.legajo) FROM medico m").uniqueResult();
+    	System.out.println(doctor.toString());
     	
     	//A través de distintas consultas obtener la cantidad de turnos que existen en
     	//estado presente y ausente entre las fechas 01/01/2024 y 01/03/2024. Luego
